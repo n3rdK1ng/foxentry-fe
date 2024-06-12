383e2761a36849cb04e2432de3efed523ce51e63
Marco <marco.velan10@gmail.com>
2024-06-12 21:17:15 +0200

    chore: add tailwindcss config & deps

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import type { LinksFunction } from '@remix-run/node'
 import {
 	Links,
 	Meta,
@@ -5,6 +6,12 @@ import {
 	Scripts,
 	ScrollRestoration,
 } from '@remix-run/react'
+
+import tailwindStyleSheetUrl from './tailwind.css?url'
+
+export const links: LinksFunction = () => [
+	{ rel: 'stylesheet', href: tailwindStyleSheetUrl },
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
