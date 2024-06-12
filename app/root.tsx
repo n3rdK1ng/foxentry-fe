cddb2c51aa9a6d87b3563ccae8838a06ebb0dceb
Marco <marco.velan10@gmail.com>
2024-06-12 21:53:35 +0200

    feat: add dark mode

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,16 @@
-import type { LinksFunction } from '@remix-run/node'
+import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node'
 import {
 	Links,
 	Meta,
 	Outlet,
 	Scripts,
 	ScrollRestoration,
+	useLoaderData,
 } from '@remix-run/react'
+import clsx from 'clsx'
+import { PreventFlashOnWrongTheme, ThemeProvider, useTheme } from 'remix-themes'
+
+import { themeSessionResolver } from '#sessions.server'
 
 import tailwindStyleSheetUrl from './tailwind.css?url'
 
@@ -13,17 +18,27 @@ export const links: LinksFunction = () => [
 	{ rel: 'stylesheet', href: tailwindStyleSheetUrl },
 ]
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export async function loader({ request }: LoaderFunctionArgs) {
+	const { getTheme } = await themeSessionResolver(request)
+	return {
+		theme: getTheme(),
+	}
+}
+
+export function App() {
+	const data = useLoaderData<typeof loader>()
+	const [theme] = useTheme()
 	return (
-		<html lang="en">
+		<html lang="en" className={clsx(theme)}>
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<Meta />
+				<PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
 				<Links />
 			</head>
 			<body>
-				{children}
+				<Outlet />
 				<ScrollRestoration />
 				<Scripts />
 			</body>
@@ -31,6 +46,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 	)
 }
 
-export default function App() {
-	return <Outlet />
+export default function AppWithProviders() {
+	const data = useLoaderData<typeof loader>()
+	return (
+		<ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+			<App />
+		</ThemeProvider>
+	)
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { MetaFunction } from '@remix-run/node'
 
+import { ModeToggle } from '#components/mode-toggle'
+
 export const meta: MetaFunction = () => {
 	return [
 		{ title: 'New Remix App' },
@@ -10,6 +12,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
 	return (
 		<div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+			<ModeToggle />
 			<h1>Welcome to Remix</h1>
 			<ul>
 				<li>
