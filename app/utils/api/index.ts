6fbf22f77d5a5038ebfcf2da649c049f48d4148a
Marco <marco.velan10@gmail.com>
2024-06-13 13:15:30 +0200

    feat: add api layer & display products

---
+++ app/utils/api/index.ts
@@ -0,0 +1,29 @@
+import { combineHeaders } from '#utils/misc'
+
+import { Product } from './types'
+
+export type ErrorResponse = {
+	error: unknown
+}
+
+const baseFetch = async <TData>(url: string, options: RequestInit = {}) => {
+	const response = await fetch(`${ENV.API_URL}/${url}`, {
+		...options,
+		headers: combineHeaders(
+			{ 'Content-Type': 'application/json' },
+			options?.headers,
+		),
+	})
+
+	const data = (await response.json()) as TData | ErrorResponse
+
+	if (typeof data === 'object' && data && 'error' in data) {
+		throw new Error(`API returned error: ${data.error}`)
+	}
+
+	return { response, data }
+}
+
+export const api = {
+	getProducts: () => baseFetch<Product[]>('products'),
+}
