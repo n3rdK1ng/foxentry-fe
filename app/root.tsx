68c07ae5899c5dc8957a96e87f5c4fccec78d33c
Marco <marco.velan10@gmail.com>
2024-06-14 23:20:34 +0200

    feat: add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,15 @@ import tailwindStyleSheetUrl from './tailwind.css?url'
 
 export const links: LinksFunction = () => [
 	{ rel: 'stylesheet', href: tailwindStyleSheetUrl },
+	{
+		rel: 'icon',
+		type: 'image/svg+xml',
+		sizes: 'any',
+		href:
+			'data:image/svg+xml,' +
+			'<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>' +
+			'<text y=%22.9em%22 font-size=%2290%22>🦊</text></svg>',
+	},
 ]
 
 export async function loader({ request }: LoaderFunctionArgs) {
